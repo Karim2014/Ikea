5b73aeb9748f4d48a0261294ac65038718d384bc
adm-sabitovka <sabitovka@shkd.bizml.ru>
2020-09-30 09:34:53 +0300

    Добавлены коментарии

--- js/main.js
+++ js/main.js
@@ -6,21 +6,26 @@ const catalogList = document.querySelector(".catalog-list");
 const subcatalogHeader = document.querySelector(".subcatalog-header");
 const btnReturn = document.querySelector(".btn-return");
 
+// Делаем overlay
 const overlay = document.createElement("div");
 overlay.classList.add('overlay');
+// добавляем в дом
 document.body.insertAdjacentHTML("beforeend", overlay);
 
+// открытие меню
 const openCatalog = () => {
   catalog.classList.add("open");
   overlay.classList.add("active")
 }
 
+// закрытие меню
 const closeCatalog = () => {
   closeSubCatalog();
   catalog.classList.remove("open");
   overlay.classList.remove("active")
 }
 
+// открыть подменю
 const openSubCatalog = event => {
   event.preventDefault();
   const itemList = event.target.closest(".catalog-list__item");
@@ -30,16 +35,19 @@ const openSubCatalog = event => {
   }
 }
 
+// закрыть подменю
 const closeSubCatalog = () => {
   subcatalog.classList.remove("subopen");
 }
 
+// доабвлены обработчики
 btnBurger.addEventListener("click", openCatalog);
 btnClose.addEventListener("click", closeCatalog);
 overlay.addEventListener("click", closeCatalog);
 catalogList.addEventListener("click", openSubCatalog);
 btnReturn.addEventListener("click", closeSubCatalog);
 
+// обработка Esc для закрытия меню
 document.addEventListener("keydown", event => {
   if (event.code === "Escape") {
     closeCatalog();
